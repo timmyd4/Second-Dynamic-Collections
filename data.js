3c32da4d747eb08267d8034ae1cbe752ee220cb8
timmyd4 <timmywilliams4665@gmail.com>
2025-10-14 13:11:42 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,194 @@
+export default [
+    {
+    poster: "https://www.themoviedb.org/t/p/w1280/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg",
+    title: "The Shawshank Redemption",
+    genre: "Drama",
+    rating: 9.3,
+    director: "Frank Darabont",
+    actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+    title: "The Godfather",
+    genre: "Crime",
+    rating: 9.2,
+    director: "Francis Ford Coppola",
+    actors: ["Marlon Brando", "Al Pacino", "James Caan"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    title: "The Dark Knight",
+    genre: "Action",
+    rating: 9.0,
+    director: "Christopher Nolan",
+    actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/vQWk5YBFWF4bZaofAbv0tShwBvQ.jpg",
+    title: "Pulp Fiction",
+    genre: "Crime",
+    rating: 8.9,
+    director: "Quentin Tarantino",
+    actors: ["John Travolta", "Uma Thurman", "Samuel L. Jackson"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/9wlYJy01XgvIhdf651FgyJkau07.jpg",
+    title: "Forrest Gump",
+    genre: "Drama",
+    rating: 8.8,
+    director: "Robert Zemeckis",
+    actors: ["Tom Hanks", "Robin Wright", "Gary Sinise"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg",
+    title: "Inception",
+    genre: "Sci-Fi",
+    rating: 8.8,
+    director: "Christopher Nolan",
+    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"]
+  },
+  {
+    poster: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    title: "Interstellar",
+    genre: "Sci-Fi",
+    rating: 8.6,
+    director: "Christopher Nolan",
+    actors: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/p96dm7sCMn4VYAStA6siNz30G1r.jpg",
+    title: "The Matrix",
+    genre: "Action",
+    rating: 8.7,
+    director: "Lana Wachowski & Lilly Wachowski",
+    actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"]
+  },
+  {
+    poster: "https://media.themoviedb.org/t/p/w600_and_h900_bestv2/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    title: "Fight Club",
+    genre: "Drama",
+    rating: 8.8,
+    director: "David Fincher",
+    actors: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/vfrQk5IPloGg1v9Rzbh2Eg3VGyM.jpg",
+    title: "Alien",
+    genre: "Horror",
+    rating: 8.4,
+    director: "Ridley Scott",
+    actors: ["Sigourney Weaver", "Tom Skerritt", "John Hurt"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg",
+    title: "Schindler's List",
+    genre: "History",
+    rating: 8.9,
+    director: "Steven Spielberg",
+    actors: ["Liam Neeson", "Ralph Fiennes", "Ben Kingsley"]
+  },
+  {
+    poster: "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    title: "Avengers: Infinity War",
+    genre: "Superhero",
+    rating: 8.4,
+    director: "Anthony Russo & Joe Russo",
+    actors: ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/sKCr78MXSLixwmZ8DyJLrpMsd15.jpg",
+    title: "The Lion King",
+    genre: "Animation",
+    rating: 8.5,
+    director: "Roger Allers & Rob Minkoff",
+    actors: ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg",
+    title: "Gladiator",
+    genre: "Action",
+    rating: 8.5,
+    director: "Ridley Scott",
+    actors: ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/nT97ifVT2J1yMQmeq20Qblg61T.jpg",
+    title: "The Departed",
+    genre: "Crime",
+    rating: 8.5,
+    director: "Martin Scorsese",
+    actors: ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
+    title: "The Avengers",
+    genre: "Superhero",
+    rating: 8.0,
+    director: "Joss Whedon",
+    actors: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/63viWuPfYQjRYLSZSZNq7dglJP5.jpg",
+    title: "Jurassic Park",
+    genre: "Adventure",
+    rating: 8.1,
+    director: "Steven Spielberg",
+    actors: ["Sam Neill", "Laura Dern", "Jeff Goldblum"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    genre: "Fantasy",
+    rating: 8.8,
+    director: "Peter Jackson",
+    actors: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg",
+    title: "The Lord of the Rings: The Return of the King",
+    genre: "Fantasy",
+    rating: 9.1,
+    director: "Peter Jackson",
+    actors: ["Elijah Wood", "Viggo Mortensen", "Sean Astin"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/5VTN0pR8gcqV3EPUHHfMGnJYN9L.jpg",
+    title: "The Lord of the Rings: The Two Towers",
+    genre: "Fantasy",
+    rating: 8.4,
+    director: "Peter Jackson",
+    actors: ["Ian McKellen", "Orlando Bloom", "Andy Serkis"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/7sfbEnaARXDDhKm0CZ7D7uc2sbo.jpg",
+    title: "Inglourious Basterds",
+    genre: "War",
+    rating: 8.3,
+    director: "Quentin Tarantino",
+    actors: ["Brad Pitt", "Christoph Waltz", "Michael Fassbender"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/2ZOzyhoW08neG27DVySMCcq2emd.jpg",
+    title: "The Prestige",
+    genre: "Drama",
+    rating: 8.5,
+    director: "Christopher Nolan",
+    actors: ["Christian Bale", "Hugh Jackman", "Scarlett Johansson"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/hzXSE66v6KthZ8nPoLZmsi2G05j.jpg",
+    title: "The Terminator",
+    genre: "Action",
+    rating: 7.5,
+    director: "James Cameron",
+    actors: ["Arnold Schwarzenegger", "Michael Biehn", "Linda Hamilton"]
+  },
+  {
+    poster: "https://www.themoviedb.org/t/p/w1280/k3mW4qfJo6SKqe6laRyNGnbB9n5.jpg",
+    title: "Predator",
+    genre: "Action",
+    rating: 7.8,
+    director: "John McTiernan",
+    actors: ["Arnold Schwarzenegger", "Carl Weathers", "Jesse Ventura"]
+  }
+]
